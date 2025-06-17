c9ecb8efd1f37ce6825a2b0893ee2ccfc8af59c1
Daniel Camacho <129220515+SeluneElegy@users.noreply.github.com>
2025-06-16 23:56:25 -0600

    Fix bullet spawn order and parent

--- gun.gd
+++ gun.gd
@@ -19,10 +19,10 @@ func disparar():
 	for i in range(dispersion):
 		var separacion = deg_to_rad((i - dispersion / 2.0) * 10)
 		var nueva_bala = bala_escena.instantiate()
+		nueva_bala.global_transform = $PuntoDisparo.global_transform
 		nueva_bala.rotation = angulo_base + separacion
 		nueva_bala.daño = jugador.estadisticas["daño"]
-		nueva_bala.global_transform = $PuntoDisparo.global_transform
-		$PuntoDisparo.add_child(nueva_bala)
+		get_tree().current_scene.add_child(nueva_bala)
 
 func _on_Timer_timeout():
 	disparar()
